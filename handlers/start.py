9ae53cd15a1d63001db0913f4da6c65da0d32884
ivanya123 <aplesnin2010@yandex.ru>
2024-07-14 00:14:55 +0300

    change inline_keyboard function, modification script choice picture

--- handlers/start.py
+++ handlers/start.py
@@ -4,10 +4,12 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, ReplyKeyboardRemove, CallbackQuery
 from keyboard.all_kb import main_kb
-from keyboard.inline_kbs import inline_link_kb, inline_canvas_base, inline_canvas_size, inline_canvas_height_and_width
+from keyboard.inline_kbs import inline_link_kb, inline_canvas_base, inline_canvas_size, inline_canvas_height_and_width,\
+    inline_choice
 import asyncio
 from aiogram.utils.chat_action import ChatActionSender
 import json
+from create_bot import bot
 
 start_router = Router()
 
@@ -17,6 +19,8 @@ class StartState(StatesGroup):
     canvas_base = State()
     canvas_size = State()
     canvas_height_and_width = State()
+    canvas_price = State()
+    # choice = State()
 
 
 @start_router.message(F.text == 'Выбрать основу холста')
@@ -36,26 +40,42 @@ async def about_me(message: Message):
 @start_router.message(F.text == 'Выбрать формат картины')
 async def start_question(message: Message, state: FSMContext):
     await state.clear()
+    with open('info.json', 'r') as f:
+        data = json.load(f)
     await message.answer("Выберите формат картины",
-                         reply_markup=inline_link_kb())
+                         reply_markup=inline_link_kb(data))
     await state.set_state(StartState.canvas_shape)
 
 
 @start_router.callback_query(F.data, StartState.canvas_shape)
 async def start_question(call: CallbackQuery, state: FSMContext):
     await state.update_data(canvas_shape=call.data)
+    with open('info.json', 'r') as f:
+        data = json.load(f)
+
+    data_state = await state.get_data()
     await call.answer("Выберите основу холста")
     await call.message.edit_reply_markup(reply_markup=None)
-    await call.message.answer('Выберите основу холста', reply_markup=inline_canvas_base())
+    if data_state["canvas_shape"] == "Нестандартный формат":
+        await call.message.answer('Выберите фигуру', reply_markup=inline_canvas_base(data, data_state))
+    else:
+        await call.message.answer('Выберите основу холста', reply_markup=inline_canvas_base(data, data_state))
     await state.set_state(StartState.canvas_base)
 
 
 @start_router.callback_query(F.data, StartState.canvas_base)
 async def start_question(call: CallbackQuery, state: FSMContext):
     await state.update_data(canvas_base=call.data)
+    with open('info.json', 'r') as f:
+        data = json.load(f)
+
+    data_state = await state.get_data()
     await call.answer("Выберите размер холста")
     await call.message.edit_reply_markup(reply_markup=None)
-    await call.message.answer('Выберите размер холста', reply_markup=inline_canvas_size())
+    if data_state["canvas_shape"] == "Нестандартный формат":
+        await call.message.answer('Только на картоне', reply_markup=inline_canvas_size(data, data_state))
+    else:
+        await call.message.answer('Выберите размер холста', reply_markup=inline_canvas_size(data, data_state))
     await state.set_state(StartState.canvas_size)
 
 
@@ -69,19 +89,57 @@ async def start_question(call: CallbackQuery, state: FSMContext):
     await call.message.edit_reply_markup(reply_markup=None)
     await call.message.answer('Выберите габарит холста', reply_markup=inline_canvas_height_and_width(data, data_state))
     await state.set_state(StartState.canvas_height_and_width)
-    print(data_state)
 
 
 @start_router.callback_query(F.data, StartState.canvas_height_and_width)
 async def start_question(call: CallbackQuery, state: FSMContext):
     await state.update_data(canvas_height_and_width=call.data)
     data_state = await state.get_data()
-    print(data_state)
+    await call.message.edit_reply_markup(reply_markup=None)
+    with open('info.json', 'r') as f:
+        data = json.load(f)
+
+    canvas_shape = data_state["canvas_shape"]
+    canvas_base = data_state["canvas_base"]
+    canvas_size = data_state["canvas_size"]
+    canvas_height_and_width = data_state["canvas_height_and_width"]
+    price = data[canvas_shape][canvas_base][canvas_size][canvas_height_and_width]
+
     text = f'Вы выбрали:\n<b>Формат:</b>{data_state["canvas_shape"]}\n' \
-           f'<b>Основу</b>: {data_state["canvas_base"]}\n' \
+           f'<b>Основа</b>: {data_state["canvas_base"]}\n' \
            f'<b>Размер</b>: {data_state["canvas_size"]}\n' \
-           f'<b>Габарит</b>: {data_state["canvas_height_and_width"]}\n' \
-           f'<tg-spoiler>Но мои картины бесценны</tg-spoiler>'
+           f'<b>Габариты</b>: {data_state["canvas_height_and_width"]}\n' \
+           f'<tg-spoiler>Но мои картины бесценны,\n' \
+           f'Для вас сделаю исключение\n' \
+           f'Цена составит: <b>{price} руб.</b></tg-spoiler>'
     await call.message.reply(text)
-    await state.set_state(StartState.canvas_height_and_width)
-    await state.clear()
+    await bot.send_message(call.from_user.id, text='Хотите заказать картину???', reply_markup=inline_choice())
+    await state.set_state(StartState.canvas_price)
+    # await state.clear()
+
+
+# @start_router.callback_query(F.data, StartState.canvas_price)
+# async def start_question(call: CallbackQuery, state: FSMContext):
+#     await state.update_data(canvas_price=call.data)
+#     data_state = await state.get_data()
+#     with open('info.json', 'r') as f:
+#         data = json.load(f)
+#
+#     await call.message.edit_reply_markup(reply_markup=None)
+#     await call.message.answer(text='Хотите заказать картину???)', reply_markup=inline_choice())
+#     await state.set_state(StartState.choice)
+
+@start_router.callback_query(F.data, StartState.canvas_price)
+async def start_question(call: CallbackQuery, state: FSMContext):
+    await state.update_data(canvas_price=call.data)
+    data_state = await state.get_data()
+
+    if data_state["canvas_price"] == "Да":
+        await call.message.edit_reply_markup(reply_markup=None)
+        await call.message.answer(text='Отлично, ждите сообщение с подробностями.\n'
+                                       'Если хоите уточнить какие то вопросы пишите мне:\n')
+        await state.clear()
+    else:
+        await call.message.edit_reply_markup(reply_markup=None)
+        await call.message.answer(text=r'Очень жаль, для выбора другой картины напишите \start')
+        await state.clear()
--- keyboard/inline_kbs.py
+++ keyboard/inline_kbs.py
@@ -2,37 +2,37 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def inline_link_kb():
-    inline_kb_list = [
-        [InlineKeyboardButton(text='Квадрат', callback_data='Квадрат')],
-        [InlineKeyboardButton(text='Прямоугольник', callback_data='Прямоугольник')],
-        [InlineKeyboardButton(text='Круг', callback_data='Круг')],
-        [InlineKeyboardButton(text='Овал', callback_data='Овал')],
-        [InlineKeyboardButton(text='Не стандартный формат', callback_data='Не стандартный формат')]
-    ]
-    return InlineKeyboardMarkup(inline_keyboard=inline_kb_list)
+def inline_link_kb(data: dict[str, dict[str, dict[str, dict[str, int]]]]) -> InlineKeyboardMarkup:
+    list_inline_kb = [[InlineKeyboardButton(text=key, callback_data=key)] for key in data]
+    return InlineKeyboardMarkup(inline_keyboard=list_inline_kb)
 
 
-def inline_canvas_base() -> InlineKeyboardMarkup:
-    inline_list = [
-        [InlineKeyboardButton(text='На подрамнике', callback_data='На подрамнике')],
-        [InlineKeyboardButton(text='На картоне', callback_data='На картоне')]
-    ]
+def inline_canvas_base(data: dict[str, dict[str, dict[str, dict[str, int]]]],
+                       data_state: dict[str, any]) -> InlineKeyboardMarkup:
+    canvas_shape = data_state['canvas_shape']
+    list_inline_kb = [[InlineKeyboardButton(text=key, callback_data=key)] for key in data[canvas_shape]]
 
-    return InlineKeyboardMarkup(inline_keyboard=inline_list)
-
-
-def inline_canvas_size() -> InlineKeyboardMarkup:
-    inline_list = [
-        [InlineKeyboardButton(text='Малый', callback_data='Малый')],
-        [InlineKeyboardButton(text='Средний', callback_data='Средний')],
-        [InlineKeyboardButton(text='Большой', callback_data='Большой')]
-    ]
-
-    return InlineKeyboardMarkup(inline_keyboard=inline_list)
+    return InlineKeyboardMarkup(inline_keyboard=list_inline_kb)
 
 
-def inline_canvas_height_and_width(data: dict[dict[dict[list[str]]]], data_state: dict[str, any]) -> InlineKeyboardMarkup:
+def inline_canvas_size(data: dict[str, dict[str, dict[str, dict[str, int]]]],
+                       data_state: dict[str, any]) -> InlineKeyboardMarkup:
+    list_inline_kb = [[InlineKeyboardButton(text=key, callback_data=key)] for key in
+                      data[data_state['canvas_shape']][data_state['canvas_base']]]
+
+    return InlineKeyboardMarkup(inline_keyboard=list_inline_kb)
+
+
+def inline_canvas_height_and_width(data: dict[dict[dict[list[str]]]],
+                                   data_state: dict[str, any]) -> InlineKeyboardMarkup:
     list_shapes = data[data_state['canvas_shape']][data_state['canvas_base']][data_state['canvas_size']]
     inline_list = [[InlineKeyboardButton(text=elem, callback_data=elem)] for elem in list_shapes]
     return InlineKeyboardMarkup(inline_keyboard=inline_list)
+
+
+def inline_choice():
+    list_choice = [
+        [InlineKeyboardButton(text="Да, возьмите мои деньги!!!", callback_data="Да"),
+         InlineKeyboardButton(text="Нет, не берите мои деньги!!!", callback_data="Нет")]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=list_choice)
